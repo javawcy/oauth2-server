019020d6bfb4fdcb4bdc94e997b3a5e952edcb27
Chongyu <javawcy@outlook.com>
2020-09-23 18:27:10 +0800

    fix: init to server

--- src/main/java/dev/lowdad/cloud/oauth2server/Oauth2ServerApplication.java
+++ src/main/java/dev/lowdad/cloud/oauth2server/Oauth2ServerApplication.java
@@ -2,8 +2,10 @@ package dev.lowdad.cloud.oauth2server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Oauth2ServerApplication {
 
     public static void main(String[] args) {
